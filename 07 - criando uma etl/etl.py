de043a3fc709722d4fabecaf85c713f39d2cc384
Felipe <felipe.r.cardoso@outlook.com>
2025-03-08 22:59:20 -0300

    add modulo de log e usando decorator

--- 07 - criando uma etl/etl.py
+++ 07 - criando uma etl/etl.py
@@ -2,11 +2,13 @@ import pandas as pd
 import os
 import glob
 from loguru import logger
+from log import log_decorator
 
 
 logger.add(r"D:\Estudos\Python\Estudo-Python\07 - criando uma etl\meus_logs.log", format="{time} {level} {message} {file} {line}", level="CRITICAL")
 # ler e consolidar os arquivos Json num dataframe.
 
+@log_decorator
 def extrair_dados(pasta: str) -> pd.DataFrame:
     arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
 
@@ -23,6 +25,7 @@ def extrair_dados(pasta: str) -> pd.DataFrame:
 
 # realizar as transformaçoes nescessárias
 
+@log_decorator
 def calcular_kpi_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df_novo = df.copy() # cria uma cópia para não alterar o df original
     df_novo['Total'] = df['Venda'] * df['Quantidade'] # cria a coluna total  no novo dataframe com o valor da venda multiplicado pela quantidade
@@ -30,6 +33,7 @@ def calcular_kpi_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
 
 # carregar os dados transformados
 
+@log_decorator
 def carregar_dados(df: pd.DataFrame, formato_saida: list): 
     for formato in formato_saida:
         if formato not in ['csv', 'parquet']:
@@ -42,6 +46,7 @@ def carregar_dados(df: pd.DataFrame, formato_saida: list):
             df.to_parquet('dados_transformados.parquet')
             logger.info('Dados salvos em Parquet')
 
+@log_decorator
 def pipeline_calcular_kpi_vendas(pasta: str, formato_saida: list):
     df = extrair_dados(pasta)
     df_calculado = calcular_kpi_total_de_vendas(df)
